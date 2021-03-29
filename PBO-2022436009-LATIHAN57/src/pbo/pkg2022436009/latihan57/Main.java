56eb823dff61e5c480bb0bef2ac2eaf093fcff34
kazuianakuam26 <81505625+kazuianakuam26@users.noreply.github.com>
2021-03-29 15:28:44 +0700

    Add files via upload

---
+++ PBO-2022436009-LATIHAN57/src/pbo/pkg2022436009/latihan57/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.pkg2022436009.latihan57;
+
+/**
+ *
+ * @author kazui
+ */
+public class Main {
+    public static void main(String[] args) {
+        Bicycle bicycle = new Bicycle();
+        Skateboard skateboard = new Skateboard();
+    }
+}
