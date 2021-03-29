03cb7f410807de0c25445b7970b29822f43f31c8
kazuianakuam26 <81505625+kazuianakuam26@users.noreply.github.com>
2021-03-29 15:26:58 +0700

    Add files via upload

---
+++ PBO-2022436009-LATIHAN55/src/pbo/pkg2022436009/latihan55/Main.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.pkg2022436009.latihan55;
+
+/**
+ *
+ * @author kazui
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        Android android = new Android("Samsung", "Android", "Grand", 3000000);
+        android.displayProduct();
+        android.setKeyStore("234ibfd3840fo");
+        System.out.println("Andriod Key Store : " + android.getKeyStore());
+
+        BlackBerry blackBerry = new BlackBerry("BlackB", "RIM", "Curve", 2000000);
+        blackBerry.displayProduct();
+        blackBerry.setPinBB("BHS249");
+        System.out.println("PIN : " + blackBerry.getPinBB());
+
+        WindowsPhone windowsPhone = new WindowsPhone("Nokia", "Win8", "Lumia", 1000000);
+        windowsPhone.displayProduct();
+        windowsPhone.setWpKeyStore("UUUQIJWORJ");
+        System.out.println("WP Key Store : " + windowsPhone.getWpKeyStore());
+    }
+}
