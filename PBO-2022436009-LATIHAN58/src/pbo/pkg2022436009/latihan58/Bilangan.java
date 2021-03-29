8bbd45ebcc5e7465c333b528753335bf106c24ca
kazuianakuam26 <81505625+kazuianakuam26@users.noreply.github.com>
2021-03-29 15:29:20 +0700

    Add files via upload

---
+++ PBO-2022436009-LATIHAN58/src/pbo/pkg2022436009/latihan58/Bilangan.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.pkg2022436009.latihan58;
+
+public class Bilangan {
+    private final int x;
+    private final int y;
+
+    public Bilangan() {
+        this.x = 3;
+        this.y = 4;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
---
+++ PBO-2022436009-LATIHAN58/src/pbo/pkg2022436009/latihan58/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.pkg2022436009.latihan58;
+
+/**
+ *
+ * @author kazui
+ */
+public class Main {
+    public static void main(String[] args) {
+        JumlahBilangan jumlahBil = new JumlahBilangan();
+        SelisihBilangan selisihBil = new SelisihBilangan();
+
+        jumlahBil.tampilHasilJumlah();
+        selisihBil.tampilHasilSelisih();
+    }
+}
