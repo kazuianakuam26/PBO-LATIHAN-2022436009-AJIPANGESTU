3920beff6acaf814a2b8c13ce6d6a449551c7846
kazuianakuam26 <81505625+kazuianakuam26@users.noreply.github.com>
2021-03-29 15:27:50 +0700

    Add files via upload

---
+++ PBO-2022436009-LATIHAN56/src/pbo/pkg2022436009/latihan56/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.pkg2022436009.latihan56;
+
+/**
+ *
+ * @author kazui
+ */
+public class Main {
+    public static void main(String[] args) {
+        Radio radio = new Radio(234);
+        radio.setNama("Radio AM");
+        System.out.println("Nama barang antik : "+radio.getNama());
+        radio.tampilUmur();
+    }
+}
---
+++ PBO-2022436009-LATIHAN56/src/pbo/pkg2022436009/latihan56/Radio.java
@@ -0,0 +1,18 @@
+package pbo.pkg2022436009.latihan56;
+
+public class Radio extends BarangAntik {
+    private String nama;
+
+    public Radio(int umur) {
+        super(umur);
+    }
+
+    
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+}
