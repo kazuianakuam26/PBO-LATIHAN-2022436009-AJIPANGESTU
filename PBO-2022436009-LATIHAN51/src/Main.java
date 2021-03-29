8a0de5f8496f2abebd9b37bd6369a145b2b739ea
kazuianakuam26 <81505625+kazuianakuam26@users.noreply.github.com>
2021-03-29 15:22:56 +0700

    Add files via upload

---
+++ PBO-2022436009-LATIHAN51/src/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kazui
+ */
+public class Main {
+    public static void main(String[] args) {
+        Rabbit rabbit = new Rabbit("Peter",false, "grass", 4, "grey" );
+        System.out.println("Hello, His name is "+rabbit.getName());
+        System.out.println(rabbit.getName()+" is Vegetarian? "+rabbit.isVegetarian());
+        System.out.println(rabbit.getName()+" eats "+rabbit.getEats());
+        System.out.println(rabbit.getName()+" has "+rabbit.getNoOfLegs()+" legs.");
+        System.out.println(rabbit.getName()+" color is "+rabbit.getColor());
+
+    }
+}
---
+++ PBO-2022436009-LATIHAN51/src/Rabbit.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kazui
+ */
+public class Rabbit extends Animal{
+    private final String color;
+    private final String name;
+
+    public Rabbit(String name, boolean veg, String food, int legs, String color) {
+        super(veg,food,legs);  //lakukan hal tambahan -> parameter yg berasal dari superClass
+        this.name = name;
+        this.vegetarian = veg;
+        this.eats = food;
+        this.noOfLegs = legs;
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
